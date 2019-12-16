689b174e99e313abc720615196272e3bb5ecfe3e
Adam Czepil <aczepil@atlassian.com>
2019-12-16 13:10:03 +1100

    Sample app using log4j2 and generating binary with native-image

---
+++ src/main/java/name/kopper/graalvm/App.java
@@ -0,0 +1,13 @@
+package name.kopper.graalvm;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class App {
+
+    private static final Logger logger = LogManager.getLogger();
+
+    public static void main(String[] args) {
+        logger.info("Hello World");
+    }
+}
